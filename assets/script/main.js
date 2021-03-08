43b0ab5111759073c0cbbdc93f7e9de126f6acc0
Marc Dobler <azrael83fr@gmail.com>
2021-03-08 11:09:28 +0100

    Update PWA init

--- assets/script/main.js
+++ assets/script/main.js
@@ -7,3 +7,11 @@ let howOldIAm = {
     return document.write(howoldyouare);
   },
 };
+
+window.onload = function () {
+  var script = document.createElement('script');
+  var firstScript = document.getElementsByTagName('script')[0];
+  script.async = true;
+  script.src = '{{'sw-register.js'|relative_url}}?v=' + Date.now();
+  firstScript.parentNode.insertBefore(script, firstScript);
+};
