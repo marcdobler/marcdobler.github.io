898e7173c870e7666fb1beb84901e023fbea6c0d
Marc DOBLER <azrael83fr@gmail.com>
2021-04-16 20:45:04 +0200

    update partial

--- assets/script/main.js
+++ assets/script/main.js
@@ -4,16 +4,27 @@ layout: null
 
 let howOldIAm = {
   inYears: function (date) {
-    let birthdate = new Date(date);
-    let now = new Date();
+    // Get date now 
+    let now = Date.now();
+    // Get date birth
+    let birthdate = new Date(date).getTime();
 
-    var howoldyouare = now.getFullYear() - birthdate.getFullYear();
+    // Make diff between date
+    let diff = now - birthdate;
+
+    // define as a date
+    let age = new Date(diff);
+    
+    // Show only years diff
+    var howoldyouare = Math.abs(age.getUTCFullYear() - 1970);
+
+    // return write value
     return document.write(howoldyouare);
   },
 };
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('{{'sw-register.js'|relative_url}}');
-  });
-}
+// if ('serviceWorker' in navigator) {
+//   window.addEventListener('load', function() {
+//     navigator.serviceWorker.register('{{'sw-register.js'|relative_url}}');
+//   });
+// }
